23610840831400343d94a11854761004abfe6d16
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-24 22:05:48 +0530

    feat: Add LecturerHttpController for managing lecturer data
    - Implemented endpoint for creating a new lecturer
    - Implemented endpoint for updating lecturer details via multipart/form-data
    - Implemented endpoint for updating lecturer details via application/json
    - Implemented endpoint for deleting a lecturer
    - Implemented endpoint for retrieving all lecturers
    - Implemented endpoint for retrieving details of a specific lecturer
    - Implemented endpoints for retrieving full-time and visiting lecturers

---
+++ back-end/EduPanel-java-api/src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -0,0 +1,40 @@
+package lk.ijse.dep11.edupanel.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/lecturers")
+@CrossOrigin
+public class LecturerHttpController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "multipart/form-data", produces = "application/json")
+    public void createNewLecturer(){}
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/{lecturer-id}", consumes = "multipart/form-data")
+    public void updateLecturerDetailsviaMultipart(@PathVariable("{lecturer-id}") Integer lecturerId){}
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/{lecturer-id}", consumes = "application/json")
+    public void updateLecturerDetailsviaApplicationjson(@PathVariable("{lecturer-id}") Integer lecturerId){}
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{lecturer-id}")
+    public void deleteLecturer(@PathVariable("{lecturer-id}") Integer lecturerId){}
+
+    @GetMapping(produces = "application/json")
+    public void getAllLecturers(){}
+
+    @GetMapping(value = "/{lecturer-id}", produces = "application/json")
+    public void getLecturerDetails(@PathVariable("{lecturer-id}") Integer lecturerId){}
+
+    @GetMapping(params = "type=full-time", produces = "application/json")
+    public void getFullTimeLecturers(){}
+
+    @GetMapping(params = "type=visiting", produces = "application/json")
+    public void getVisitingLecturers(){}
+
+
+}
