bffb232418c0975cc0dfdd8227a127659dc03bef
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-25 22:18:35 +0530

    Add WebAppConfig class for configuring Spring MVC

--- back-end/EduPanel-java-api/src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ back-end/EduPanel-java-api/src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -1,12 +1,26 @@
 package lk.ijse.dep11.edupanel;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ComponentScan
 @EnableWebMvc
 @Configuration
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        WebMvcConfigurer.super.addFormatters(registry);
+    }
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver(){
+        return  new StandardServletMultipartResolver();
+    }
 
 }
